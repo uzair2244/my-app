6637ec7174b2b43d51783a0e60724d4dd232f749
uzair2244 <uzirao17@gmail.com>
2024-03-13 14:47:47 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ async function headers() {
     return [
         {
             // matching all API routes
-            source: "/api/*",
+            source: "/api/:path*",
             headers: [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
