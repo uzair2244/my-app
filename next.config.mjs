58fa23cd94fa9b0ffc16743431f37259570265ed
uzair2244 <uzirao17@gmail.com>
2024-03-13 14:43:59 -0500

    config file altered

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ async function headers() {
     return [
         {
             // matching all API routes
-            source: "/api/:path*",
+            source: "/api/*",
             headers: [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
